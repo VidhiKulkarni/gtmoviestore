0d979d7dd8fcff5ae1b158c0c296956d46d25aab
smunigunti3 <msiri006@gmail.com>
2025-02-06 15:44:47 -0500

    Chapter 7 and 8 Instructions

--- gtmoviestore/movies/admin.py
+++ gtmoviestore/movies/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Movie
-admin.site.register(Movie)
+# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
