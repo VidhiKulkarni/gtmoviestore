2cdf23d663a7e950c13129459f7483f09c66175a
VidhiKulkarni <32562016+VidhiKulkarni@users.noreply.github.com>
2025-02-11 18:43:38 -0800

    fixed orders

---
+++ gtmoviestore/movies/utils.py
@@ -0,0 +1,53 @@
+import requests
+from django.conf import settings
+
+def fetch_movie_from_omdb(movie_name):
+    api_key = settings.OMDB_API_KEY  # Now using the direct API key from settings.py
+    url = f"http://www.omdbapi.com/?t={movie_name}&apikey={api_key}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data['Response'] == 'True':
+            return {
+                'name': data.get('Title', 'Unknown'),
+                'description': data.get('Plot', 'No description available'),
+                'image': data.get('Poster', ''),  # Map OMDb poster to image
+                'price': 15.00
+            }
+        else:
+            return None
+    else:
+        raise Exception("Error fetching data from OMDb API")
+
+
+import requests
+from django.conf import settings
+
+
+def fetch_movies_list(search_term):
+    api_key = settings.OMDB_API_KEY
+    url = f"http://www.omdbapi.com/?s={search_term}&apikey={api_key}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data['Response'] == 'True':
+            return data['Search']  # Return a list of movie search results
+    return []
+
+def fetch_movie_details(movie_title):
+    api_key = settings.OMDB_API_KEY
+    url = f"http://www.omdbapi.com/?t={movie_title}&apikey={api_key}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data['Response'] == 'True':
+            return {
+                'name': data.get('Title', 'Unknown'),
+                'description': data.get('Plot', 'No description available'),
+                'image': data.get('Poster', ''),
+                'price': 15.00  # Default price
+            }
+    return None
