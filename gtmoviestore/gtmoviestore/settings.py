227e5ef87a43420e70d125ab0cb1f0015b46e146
VidhiKulkarni <32562016+VidhiKulkarni@users.noreply.github.com>
2025-02-16 12:39:35 -0800

    Update settings.py for deployment

--- gtmoviestore/gtmoviestore/settings.py
+++ gtmoviestore/gtmoviestore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-w#vjrhc99bcp)lieho^y!l2!yqa#aib8!q#ul!kbr8+#udvuqy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
